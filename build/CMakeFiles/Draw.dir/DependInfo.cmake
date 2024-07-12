35f54845f88ae3178e8a975a1ccc45a51bba9dc5
ferrandusqui31 <ferran.marpas@gmail.com>
2024-07-12 19:48:57 +0200

    added cmake

---
+++ build/CMakeFiles/Draw.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ferran/Draw/Color.cpp" "CMakeFiles/Draw.dir/Color.cpp.o" "gcc" "CMakeFiles/Draw.dir/Color.cpp.o.d"
+  "/home/ferran/Draw/Draw.cpp" "CMakeFiles/Draw.dir/Draw.cpp.o" "gcc" "CMakeFiles/Draw.dir/Draw.cpp.o.d"
+  "/home/ferran/Draw/main.cpp" "CMakeFiles/Draw.dir/main.cpp.o" "gcc" "CMakeFiles/Draw.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
